40976708f2ba07bb780cf97aaa50035541726e26
akirraint@gmail.com <Nickel12345>
2022-03-28 00:23:44 +0300

    - version updated

--- handlers.go
+++ handlers.go
@@ -94,7 +94,7 @@ var events = map[string]event{
 }
 
 func VersionHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "{ \"version\": \"3.0\" }")
+	fmt.Fprintf(w, "{ \"version\": \"3.1\" }")
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
